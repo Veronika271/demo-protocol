996fe1f26bd41f9a89fa239af8d6ffe3386ad9ac
unknown <annika.vika@gmail.com>
2025-01-29 15:38:15 -0500

    change config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://github.com/Veronika271/reproschema-ui',
+  githubSrc: 'https://raw.githubusercontent.com/Veronika271/demo-protocol/main/DemoProtocol/DemoProtocol_schema',
   banner: 'This is a demonstration protocol for ReproSchema.',
   startButton: 'Join',
   assetsPublicPath: '/demo-protocol/',
